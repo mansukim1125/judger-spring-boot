3dae31c08caff3ef64b6b7cbc204727f9bf91765
joonseok kim <mansukim1125@gmail.com>
2025-06-02 20:50:30 +0900

    initial commit

---
+++ src/main/java/com/mansu/judger/JudgerApplication.java
@@ -0,0 +1,13 @@
+package com.mansu.judger;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JudgerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JudgerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mansu/judger/JudgerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mansu.judger;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JudgerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
